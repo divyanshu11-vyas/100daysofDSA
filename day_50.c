0262be45f8318ab4aea9895e8a4acdcec7b8084c
DIVA <iamdiv2006@gmail.com>
2026-03-22 23:38:02 +0530

    Add files via upload

---
+++ day_50.c
@@ -0,0 +1,73 @@
+// Problem: BST Search
+
+// Implement the solution for this problem.
+
+// Input:
+// - Input specifications
+
+// Output:
+// - Output specifications
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure for BST node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// Insert into BST
+struct Node* insert(struct Node* root, int data) {
+    if (root == NULL)
+        return createNode(data);
+
+    if (data < root->data)
+        root->left = insert(root->left, data);
+    else
+        root->right = insert(root->right, data);
+
+    return root;
+}
+
+// Search in BST
+int search(struct Node* root, int key) {
+    if (root == NULL)
+        return 0;
+
+    if (root->data == key)
+        return 1;
+    else if (key < root->data)
+        return search(root->left, key);
+    else
+        return search(root->right, key);
+}
+
+int main() {
+    int n, x, key;
+    struct Node* root = NULL;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        root = insert(root, x);
+    }
+
+    scanf("%d", &key);
+
+    if (search(root, key))
+        printf("Found\n");
+    else
+        printf("Not Found\n");
+
+    return 0;
+}

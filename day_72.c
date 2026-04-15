3b5ec5ded7bec59cab08cab3669090bbde693f7a
DIVA <iamdiv2006@gmail.com>
2026-04-15 23:51:21 +0530

    Add files via upload

--- day_72.c
+++ day_72.c
@@ -12,3 +12,29 @@
 
 // Sample Output
 // e
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[1000];
+    scanf("%s", s);
+
+    int visited[26] = {0};  // for 'a' to 'z'
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        int index = s[i] - 'a';
+
+        if (visited[index] == 1) {
+            // second occurrence found
+            printf("%c\n", s[i]);
+            return 0;
+        }
+
+        visited[index] = 1;
+    }
+
+    // no repeated character
+    printf("-1\n");
+
+    return 0;
+}
